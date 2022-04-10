500e85bdb5188b7f44a706843ae0af010cb80bb4
MissVeronica <veronica@tranza.se>
2022-04-10 04:08:20 +0200

    Update local-language-um-backend.php

--- local-language-um-backend.php
+++ local-language-um-backend.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name:     Ultimate Member - Local Language Backend
- * Description:     Extension to Ultimate Member for Addition of Browser or User Profile Local Language support to UM Backend.
+ * Description:     Extension to Ultimate Member for Addition of Browser or User Profile Local Language support to UM Back-end and Front-end.
  * Version:         1.0.0
  * Requires PHP:    7.4
  * Author:          Miss Veronica
@@ -12,17 +12,38 @@
  * Domain Path:     /languages
  * UM version:      2.3.1
  */
+
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 add_filter( 'um_language_locale', 'my_um_language_locale_fix', 10, 1 );
 
 function my_um_language_locale_fix( $language_locale ) {
 
-    $browser_language = str_replace( '-', '_', substr( $_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 5 ));
-    if( in_array( $browser_language, get_available_languages())) {
-        return $browser_language;
+    if( isset( $_SERVER['HTTP_ACCEPT_LANGUAGE'] ) && !empty( $_SERVER['HTTP_ACCEPT_LANGUAGE'] )) {
+        $browser_language = str_replace( '-', '_', substr( $_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 5 ));
+        if( in_array( $browser_language, get_available_languages())) {
+            return $browser_language;
+        }
     }
     
     require_once( ABSPATH . 'wp-includes/pluggable.php' );
     return get_user_locale();
+}
+
+add_filter( 'locale', 'um_get_user_locale_custom', 10, 1 );
+
+function um_get_user_locale_custom( $locale ) {
+
+    global $current_user;
+
+    if( isset( $_SERVER['HTTP_ACCEPT_LANGUAGE'] ) && !empty( $_SERVER['HTTP_ACCEPT_LANGUAGE'] )) {
+        $browser_language = str_replace( '-', '_', substr( $_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 5 ));
+        if( in_array( $browser_language, get_available_languages())) {
+            return $browser_language;
+        }
+    }
+
+    if( isset( $current_user->ID )) return get_user_locale();
+
+    return $locale;
 }
